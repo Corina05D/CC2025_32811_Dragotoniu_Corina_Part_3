ba67ff6f472b9d71583423304cedd8bdfaef055a
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2026-01-21 21:48:10 +0200

    add new packages

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/#!/bin/bash
+# intră în folderul frontend
 
-#install dependencies
+# instalează toate dependențele
 pip install -r requirements.txt
 
-# start Streamlit 
+# pornește aplicația Streamlit
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
