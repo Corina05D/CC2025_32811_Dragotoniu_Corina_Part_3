617cc54ea587ac0c8f594db487c011e84d654859
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2026-01-22 21:07:46 +0200

    change startup.sh for frontend

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#install dependencies
 pip install -r requirements.txt
 
+
+# start application FastAPI with gunicorn + uvicorn
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
--- frontend/startup.sh
+++ frontend/startup.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-# Creează și activează mediul virtual dacă nu există
-if [ ! -d "./antenv" ]; then
-    python3 -m venv antenv
-fi
 
-# Activează mediul virtual
-source ./antenv/bin/activate
-
-# Instalează dependențele
-pip install --upgrade pip
+#install dependencies
 pip install -r requirements.txt
 
-# pornește aplicația Streamlit
+# start Streamlit 
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
