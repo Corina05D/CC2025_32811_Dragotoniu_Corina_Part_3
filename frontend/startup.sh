09ce1cefecfa297ec7c2f0a7ab21b4cf8982818a
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2026-01-22 21:28:27 +0200

    final deploy

--- frontend/startup.sh
+++ frontend/startup.sh
@@ -1,24 +1,9 @@
-#!/bin/bash
-set -e  # oprește scriptul dacă apare o eroare
 
-# Directorul aplicației
-APP_DIR="/home/site/wwwroot"
-cd $APP_DIR
+#!/bin/#!/bin/bash
+# intră în folderul frontend
 
-# Numele mediului virtual
-VENV_NAME="antenv"
-
-# Creează mediul virtual dacă nu există
-if [ ! -d "$APP_DIR/$VENV_NAME" ]; then
-    python3 -m venv $VENV_NAME
-fi
-
-# Activează mediul virtual
-source "$APP_DIR/$VENV_NAME/bin/activate"
-
-# Instalează / reinstalează dependențele
-pip install --upgrade pip
+# instalează toate dependențele
 pip install -r requirements.txt
 
-# start Streamlit 
+# pornește aplicația Streamlit
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
