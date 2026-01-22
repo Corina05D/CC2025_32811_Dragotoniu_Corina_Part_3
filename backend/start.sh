e2f4bd9fe77c6d0868d7e05f02406aa48ffc68db
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2026-01-22 13:09:51 +0200

    new change

--- backend/start.sh
+++ backend/start.sh
@@ -2,4 +2,4 @@
 
 #pip install -r requirements.txt
 
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
